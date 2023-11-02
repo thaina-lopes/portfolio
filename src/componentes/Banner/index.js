af866a9bccfa67902540c22c8ca0294e550c48c2
Thaina <thainalopes@gmail.com>
2023-11-02 20:05:13 -0300

    Incluindo ícones de redes sociais

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,5 +1,6 @@
 import styles from "./Banner.module.css";
 import minhaFoto from "assets/imagem_banner02.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Banner() {
   return (
@@ -7,8 +8,23 @@ export default function Banner() {
       <div className={styles.apresentacao}>
         <h1 className={styles.titulo}>Thainá Lopes</h1>
         <p className={styles.paragrafo}>Desenvolvedora Front-end</p>
+        <div className={styles.iconesContainer}>
+          <a
+            href="https://github.com/thaina-lopes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={32} className={styles.icones} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/thaina-lops/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin size={32} className={styles.icones} />
+          </a>
+        </div>
       </div>
-
       <div className={styles.imagens}>
         <img
           className={styles.minhaFoto}
--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,6 +1,5 @@
 import SobreMim from "paginas/SobreMim";
 import Projetos from "paginas/Projetos";
-import styles from "./Inicio.module.css";
 import Setas from "componentes/Setas";
 
 export default function Inicio() {
