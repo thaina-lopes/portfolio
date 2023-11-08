b75e6a2218ae18e886c69c6efe577bb738d34095
Thaina <thainalopes@gmail.com>
2023-11-08 18:27:30 -0300

    Adicionando link para ir para os projetos

--- src/paginas/Projetos/index.js
+++ src/paginas/Projetos/index.js
@@ -5,7 +5,7 @@ import PostCard from "componentes/PostCard";
 
 export default function Projetos() {
   return (
-    <section className={styles.projetosContainer}>
+    <section className={styles.projetosContainer} id="projetos">
       <h2 className={styles.titulo}>Projetos</h2>
       <ul className={styles.posts}>
         {posts.map((post) => (
